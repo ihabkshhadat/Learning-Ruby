a6a1a30a6cba691de106e0a8f9572447a523b18e
Ihab Shhadat <ishhadat@everconnect.com>
2022-07-18 13:39:11 +0300

    first commit

--- string_composer.rb
+++ string_composer.rb
@@ -1,37 +1,34 @@
 class StringComposer
     def initialize(input_str)
-        begin
-            string_validation(input_str)
-            puts composed(input_str)
-            rescue =>exception
-            puts exception   
-        end
+        @input_str=input_str
     end
 
-    def string_validation(input_str)
-        if input_str.nil? or input_str.empty?
-            raise "Input Value Can't Be Null"
-        elsif input_str =~ /[\d+|\s+]/
-            raise "Input Value Can't Contain Number Or WhiteSpace" 
+    def string_validation()
+        if @input_str.nil? or @input_str.empty? or @input_str =~ /[\d+|\s+]/
+            return false
         end
+        return true
     end
 
-    def composed(input_str)
-        char = input_str[0]
-        count=1
-        result = ""
-        for index in 1...input_str.length
-            if char==input_str[index]
-                count+=1
-            else
-                result += "#{char}#{count}"
-                count=1
-                char=input_str[index]
+    def composed()
+        if string_validation
+            char = @input_str[0]
+            count=1
+            result = ""
+            for index in 1...@input_str.length
+                if char==@input_str[index]
+                    count+=1
+                else
+                    result += "#{char}#{count}"
+                    count=1
+                    char=@input_str[index]
+                end
             end
+            return result
         end
-        return result
+        return "Input Value Not a Valid Input"
     end
 end
 
 input_val = gets.chomp()
-StringComposer.new(input_val)
+puts StringComposer.new(input_val).composed
